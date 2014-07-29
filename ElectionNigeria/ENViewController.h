119b28c42cb39d8057a278f8dbe562bee5b94509
vicky13vikas <vikas4great@gmail.com>
2014-07-29 09:02:24 +0530

    Initial Project Steup

---
+++ ElectionNigeria/ENViewController.h
@@ -0,0 +1,13 @@
+//
+//  ENViewController.h
+//  ElectionNigeria
+//
+//  Created by Vikas Kumar on 29/07/14.
+//  Copyright (c) 2014 Vikas Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ENViewController : UIViewController
+
+@end
---
+++ ElectionNigeria/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ElectionNigeria
+//
+//  Created by Vikas Kumar on 29/07/14.
+//  Copyright (c) 2014 Vikas Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ENAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ENAppDelegate class]));
+    }
+}
